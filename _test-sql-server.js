e27c1fb633b5091ba640462df5413fbc23558425
SERIMPA <serimpa86@gmail.com>
2026-05-24 21:41:49 -0300

    Remove test file

--- _test-sql-server.js
+++
@@ -1,6 +0,0 @@
-const initSqlJs = require('./node_modules/sql.js');
-initSqlJs({ locateFile: () => './node_modules/sql.js/dist/sql-wasm.wasm' }).then(SQL => {
-  const db = new SQL.Database();
-  try { db.exec("CREATE TABLE test1(id TEXT PRIMARY KEY, created_at TEXT DEFAULT CURRENT_TIMESTAMP)"); console.log('CURRENT_TIMESTAMP: OK'); } catch(e) { console.log('CURRENT_TIMESTAMP ERROR:', e.message); }
-  try { db.exec("CREATE TABLE test2(id TEXT PRIMARY KEY, created_at TEXT DEFAULT (datetime('now')))"); console.log('datetime now: OK'); } catch(e) { console.log('datetime now ERROR:', e.message); }
-});
